893e7637988478c0ace645bd847fa7d753b75992
Mercy <mercycheruiyot2013@gmail.com>
2022-09-27 21:09:32 +0300

    Implement Directors

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { directors } from "../data";
+import NameMovies from "./NameMovies";
 
 function Directors() {
+  const directorsList= directors.map(director => 
+    <NameMovies key={director.name}
+     director={director}/>)
   return <div>
     <h1>Directors Page</h1>
+    {directorsList}
   </div>;
 }
 
